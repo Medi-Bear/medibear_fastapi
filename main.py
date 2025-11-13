f8c374ed96a491dc692380379261c1addd27a4bf
이승권 <s2ngkwon2@gmail.com>
2025-11-13 17:32:04 +0900

    Revert "Merge pull request #16 from Medi-Bear/stress"
    This reverts commit b8daeb7866a1fb539a5bb5e416a446f03c20029a, reversing
    changes made to 9c0050118f006dc3828613cfe6ddff8604f23108.

--- main.py
+++ main.py
@@ -3,10 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.routers.calorie import calorie_router
 from app.routers.ophtha import ophtha_router
 from pydantic import BaseModel
-# 
-from app.routers import predict
+# from app.routers import predict
 from app.routers.sleep_router import sleep_api, sleepchat_api
-from app.routers.stresscare.stress_router import router as stresscare_router
 import uvicorn
 
 app = FastAPI()
@@ -20,16 +18,12 @@ app.add_middleware(
 )
 
 # 라우터 등록
-app.include_router(predict.router, prefix="/predict", tags=["prediction"])
+# app.include_router(predict.router, prefix="/predict", tags=["prediction"])
 app.include_router(calorie_router.router, prefix="/calorie", tags=["calorie_prediction"])
 app.include_router(ophtha_router.router, prefix="/ophtha", tags=["ophtha"])
+# app.include_router(predict.router, prefix="/predict", tags=["prediction"])
 app.include_router(sleep_api.router)
 app.include_router(sleepchat_api.router)
-app.include_router(stresscare_router, prefix="/stress", tags=["stresscare"])
-
-@app.get("/__routes__")
-def __routes__():
-    return {"paths": [getattr(r, "path", None) for r in app.routes]}
 
 # reload=True : 코드 변경 시 서버 자동 재시작
 if __name__ == "__main__":
