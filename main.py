5a3aa1c0519de7edb6b39757c658691e7cd6bc3b
Bluemoon105 <lyj69641@gmail.com>
2025-11-14 15:56:28 +0900

    temp

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from app.routers.ophtha import ophtha_router
 from pydantic import BaseModel
 # from app.routers import predict
 from app.routers.sleep_router import sleep_api, sleepchat_api
+from app.routers.stresscare.stress_router import router as stresscare_router
 import uvicorn
 
 app = FastAPI()
@@ -24,9 +25,10 @@ app.include_router(ophtha_router.router, prefix="/ophtha", tags=["ophtha"])
 # app.include_router(predict.router, prefix="/predict", tags=["prediction"])
 app.include_router(sleep_api.router)
 app.include_router(sleepchat_api.router)
+app.include_router(stresscare_router)
 
 # 운동 라우터 
-app.include_router()
+# app.include_router()
 
 # reload=True : 코드 변경 시 서버 자동 재시작
 if __name__ == "__main__":
